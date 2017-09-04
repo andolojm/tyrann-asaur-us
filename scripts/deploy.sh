d6f9199fe8d1672e06134c3d79a79083187123b3
Jeff Andolora <jeeeeefff@gmail.com>
2017-09-04 18:30:22 +0000

    Add a deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,3 @@
+webpack-cli
+sudo cp -r dist /var/www/html/
+sudo cp index.html /var/www/html/
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,5 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update
 sudo apt-get install yarn
 yarn install
+npm install -g webpack-cli
+scripts/deploy.sh
