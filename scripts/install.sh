8798475704c381797cce011d69a9a478c44c385b
Jeff Andolora <jeeeeefff@gmail.com>
2017-10-07 20:55:02 +0000

    Improve install script to work on fresh ubuntu 16.04 instance

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,12 @@
+sudo apt-get update
+sudo apt-get install apache2
+sudo ufw allow 'Apache Full'
+sudo apt-get install npm
+sudo npm install -g n
+sudo n stable
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
 sudo apt-get install yarn
 yarn install
-npm install -g webpack-cli
+sudo npm install -g webpack-cli
 scripts/deploy.sh
